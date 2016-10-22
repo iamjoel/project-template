1dfd2c19b91532d8875f19e793eef248da89d369
joel <iamjoel007@gmail.com>
2016-10-22 22:11:37 +0800

    rewrite alert and confirm

--- src/views/demo/component.vue
+++ src/views/demo/component.vue
@@ -1,46 +1,37 @@
 <template>
 <div class="main-container">
-  <h2>弹出框</h2>
-  <button class="modal-trigger-btn" @click="showModal=true">模态框</button>
-  <modal v-if="showModal" width="250px" @close="showModal = false">
+  <button @click="showAlert=true">弹出 Alert框</button>
+  <alert v-if="showAlert" width="250px" @close="showAlert = false">
     <!-- <span slot="header">自定义头部</span> -->
     <div slot="body">自定义内容</div>
-  </modal>
-  <!-- <button class="modal-trigger-btn" @click="showAlert=true">Alert框</button>
-  <alert :show.sync="showAlert">
-    <div slot="msg">发卡成功</div>
   </alert>
-  <button class="modal-trigger-btn" @click="showConfirm=true">确认框</button>
-  <div class="confirm-msg" v-show="confirmResult">已确认</div>
-  <confirm :show.sync="showConfirm" :confirmed.sync="confirmResult" confirmCallabd="">
-    <div slot="msg">会员卡注销不可恢复，是否继续
-      <div>
-  </confirm> -->
+  <button @click="showConfirm=true">弹出 Confirm框</button>
+  <confirm v-if="showConfirm" @close="showConfirm = false" @confirm="confirmed()">
+    <div slot="body">准备好了？</div>
+  </confirm>
 </div>
 
 </template>
 
 <script>
-  var Modal = require('component/Modal.vue');
-  // require('component/alert');
-  // require('component/confirm');
+  var Alert = require('component/Alert.vue')
+  var Confirm = require('component/Confirm.vue')
+
   export default {
     components: {
-      Modal
+      Alert,
+      Confirm
     },
     data () {
       return {
-        showModal:false,
         showAlert: false,
-        showConfirm: false,
-        confirmResult: false
+        showConfirm: false
       }
     },
-    watch: {
-      confirmResult (val, oldVal) {
-        if(val === true){
-          console.log('confirmed');
-        }
+    methods: {
+      confirmed() {
+        console.log('confirmed!')
+        this.showConfirm = false
       }
     }
   }
