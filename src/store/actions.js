d704a942670ad4a7bcc9ba0e0929e5568eb868a8
金伟强 <iamjoel007@gmail.com>
2016-10-27 16:52:25 +0800

    adds

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,6 @@
 import * as types from './mutation-types'
 import * as song from 'api/song'
 
-
 export const fetchSongList = ({ commit }, queryObj) => {
   song.fetchList(queryObj).then(songList => {
     commit(types.FETCH_SONG_LIST, songList)
