378fd36cb0cd08254ef971b8270a39ec81dc7cbd
金伟强 <iamjoel007@gmail.com>
2016-10-28 18:05:40 +0800

    adds

---
+++ src/views/music/song/edit.vue
@@ -0,0 +1,17 @@
+<template>
+  <form action="">
+    <label for="">
+      姓名
+      <input type="text">
+    </label>
+  </form>
+</template>
+
+<script>
+  export default {
+    created() {
+      debugger
+      this.$route.param.id
+    }
+  }
+</script>
