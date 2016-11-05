304ae5e1ade8683b04dba5e5cd437683c3fdff5f
joel <iamjoel007@gmail.com>
2016-11-05 20:51:11 +0800

    hide vue compress warning

--- TODO.md
+++ TODO.md
@@ -1,4 +1,5 @@
 ## TODO
 * [precss](https://github.com/jonathantneal/precss) 和 [postcss-scss](https://github.com/postcss/postcss-scss) 到底哪个好。
 * Grid的操作，最多显示3个，更多的放在收起来
-* Expanding 那效果有问题。。。
+* 组件
+  * 表单验证组件
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -4,17 +4,30 @@ const baseWebpackConfig = require('./webpack.base.conf')
 var path = require('path')
 
 module.exports = merge(baseWebpackConfig, {
-  // plugins: [new webpack.optimize.CommonsChunkPlugin({
-  //   name: 'vendor',
-  //   minChunks(module, count) {
-  //     // any required modules inside node_modules are extracted to vendor
-  //     return (
-  //       module.resource &&
-  //       /\.js$/.test(module.resource) &&
-  //       module.resource.indexOf(
-  //         path.join(__dirname, '../node_modules')
-  //       ) === 0
-  //     )
-  //   }
-  // })]
+  plugins: [
+    // 去除warning 提示
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'vendor',
+    //   minChunks(module, count) {
+    //     // any required modules inside node_modules are extracted to vendor
+    //     return (
+    //       module.resource &&
+    //       /\.js$/.test(module.resource) &&
+    //       module.resource.indexOf(
+    //         path.join(__dirname, '../node_modules')
+    //       ) === 0
+    //     )
+    //   }
+    // })
+  ]
 })
