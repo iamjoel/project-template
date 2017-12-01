5bf302afae1e28cf54a0e9f22cf404dccd586c9f
joel <iamjoel007@gmail.com>
2017-12-01 15:17:06 +0800

    enchance pics

--- src/components/remote-select/Index.vue
+++ src/components/remote-select/Index.vue
@@ -36,11 +36,6 @@ export default {
   },
   methods: {
     fetch(name) {
-      // debugger
-    //   this.list = [{
-    //     uuid: '1',
-    //     name: 'test'
-    //   }]
       var otherQuery
       if(typeof this.otherQuery === 'function') {
         otherQuery = this.otherQuery()
