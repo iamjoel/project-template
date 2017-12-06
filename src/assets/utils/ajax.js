801f38f8a85629092daf0c9f59cb313e10e7879d
joel <iamjoel007@gmail.com>
2017-12-06 14:05:28 +0800

    enchance interceptor

--- src/assets/utils/ajax.js
+++ src/assets/utils/ajax.js
@@ -1,24 +1,21 @@
 import axios from 'axios'
 // localStorage.setItem('sc-sessionid', Math.random()) // 测试
 import {Message} from 'element-ui'
-import {ERROR_CODE_MAP, oauth2, urls} from '@/setting'
-import router from '@/router'
 
 axios.interceptors.response.use(function (response) {
   var data = response.data
   var config = response.config
-  var errcode = data.errcode
-  if(errcode == 13) { // session 超时，跳登录页
-    router.push('/login')
-  } else if(errcode != undefined && errcode != 0) {
-    if(!config.notShowError) { // 调用的时候设置的
-      var errmsg = ERROR_CODE_MAP[errcode] || data.errmsg
-      Message({
-        showClose: true,
-        message: errmsg,
-        type: 'error'
-      })
+  var errorCode = data.errorCode
+  if(errorCode) {
+    if(errorCode == 1) {
+      location.href = 'login.html'
+      return 
     }
+    Message({
+      message: data.errorMessage || '未知错误',
+      type: 'error'
+    })
+    return Promise.reject()
   }
   return response;
 }, function (error) {
