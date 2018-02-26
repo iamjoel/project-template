909c4aba536b995bb785ce373dab6c361aad7d42
joel <iamjoel007@gmail.com>
2018-02-26 17:54:31 +0800

    add stats

--- admin/dist/static/js/element.f1f6fa719739cd05fd74.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{334:function(n,o,p){n.exports=p(8)}},[334]);
-//# sourceMappingURL=element.f1f6fa719739cd05fd74.js.map
--- admin/dist/static/js/vue.2833255028d273918a8d.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{335:function(n,o,p){n.exports=p(2)}},[335]);
-//# sourceMappingURL=vue.2833255028d273918a8d.js.map
