22e18cbe3dbc9a441757143f9c22030592fbe21d
joel <iamjoel007@gmail.com>
2018-03-31 22:58:03 +0800

    add generate code des

--- admin/src/setting.js
+++ admin/src/setting.js
@@ -97,28 +97,44 @@ export const pageConfig = {
         key: 'name'
       },{
         label: '歌手',
-        key: 'singer.name'
+        key: 'singer.name',
       },{
         label: '类型',
         key: 'type',
         formatFn: 'formatType'
+        // isShow: 'fnName'
       }],
-      operate: [{
-        name: 'add',
-      }, {
-        name: 'edit',
-        role: ['admin']
-      }, {
-        name: 'delete',
-        role: ['admin']
-      }],
+      operate: {
+        add: {
+          isShow: true,
+        },
+        edit: {
+          isShow: 'isShowEdit'
+        },
+        detail: {
+          isShow: false
+        },
+        delete: {
+          isShow: ['admin']
+        }
+      },
       searchCondition: [{
         label: '歌曲名称',
         key: 'name'
       }],
       fn: [{
         name: 'formatType',
-        fn: new Function('row', 'utils' , 'return this.config.urlKey + row.type')
+        fn: new Function('row', 'return this.config.urlKey + row.type')
+      }, {
+        name: 'isShowEdit',
+        fn: new Function('return this.$store.state.role == "admin"')
+      }]
+    },
+    detail: {
+      model: [{
+        name: '',
+        key: '',
+        dataType: 'string'
       }]
     }
   }
