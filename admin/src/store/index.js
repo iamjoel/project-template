e9884a5e40ded57392e26b69a0aa79f02a87d8d8
joel <iamjoel007@gmail.com>
2018-04-02 10:01:07 +0800

    enchance

--- admin/src/store/index.js
+++ admin/src/store/index.js
@@ -9,6 +9,15 @@ Vue.use(Vuex)
 const debug = process.env.NODE_ENV !== 'production'
 
 const state = {
+  /*
+  * 基础数据没加载好前，页面显示loading
+  * 基础数据包括：菜单，字典，实体，页面配置数据
+  */
+  hasLoadBasicData: false,
+  navMenu: [], // 菜单
+  dict: [], // 字典
+  entities: [], // 实体
+  pagesConfig: {}, // 页面配置
   user: {
     id: null,
     name: null,
