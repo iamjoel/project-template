dddffa305057f8f23f126c8e6b3f1f8547ef99ef
joel <iamjoel007@gmail.com>
2018-05-17 07:31:36 +0800

    add lazy and infint load

--- mobile/src/views/classify-list/Index.vue
+++ mobile/src/views/classify-list/Index.vue
@@ -2,16 +2,34 @@
   <div class="main">
     <van-tabs v-model="active">
       <van-tab  title="新品">
-        <div class="ly ly-multi ly-j item-wrap">
-          <div v-for="i in 9" :key="i" class="item">
-            <img src="http://via.placeholder.com/200x100" alt="" class="img" />
-            新品{{i}}
+        <van-list
+            v-model="newItem.isLoading"
+            :finished="newItem.isFinished"
+            @load="onLoad('newItem')"
+          >
+          <div class="ly ly-multi ly-j item-wrap">
+              <div v-for="(item,i) in newItem.list" :key="i" class="item">
+                <img v-lazy="'http://via.placeholder.com/200x100'" alt="" class="item__img" />
+                <div class="mt-10rem">新品{{i}}</div>
+                <div class="item__price">￥23</div>
+              </div>
           </div>
-          
-        </div>
+        </van-list>
       </van-tab>
       <van-tab  title="热销">
-        xx2
+        <van-list
+            v-model="hotItem.isLoading"
+            :finished="hotItem.isFinished"
+            @load="onLoad('hotItem')"
+          >
+          <div class="ly ly-multi ly-j item-wrap">
+            <div v-for="(item,i) in hotItem.list" :key="i" class="item">
+              <img v-lazy="'http://via.placeholder.com/200x100'" alt="" class="item__img" />
+              <div class="mt-10rem">热销{{i}}</div>
+              <div class="item__price">￥23</div>
+            </div>
+          </div>
+        </van-list>
       </van-tab>
     </van-tabs>
   </div>
@@ -22,23 +40,54 @@
 export default {
   data() {
     return {
-      active: 0
+      active: 0,
+      newItem: {
+        list: new Array(10),
+        isLoading: false,
+        isFinished: false
+      },
+      hotItem: {
+        list: new Array(10),
+        isLoading: false,
+        isFinished: false
+      },
     }
   },
   methods: {
-    
-  }
+    onLoad(type) {
+      var list = this[type].list
+      setTimeout(() => {
+        for (let i = 0; i < 6; i++) {
+          list.push('');
+        }
+        this[type].isLoading = false;
+
+        if (list.length >= 40) {
+          this[type].isFinished = true;
+        }
+      }, 500);
+    }
+  },
+  
 }
 </script>
 
 <style scoped>
   .item-wrap {
-    padding: 0 3.3%;
+    padding: 0 .2rem;
   }
   .item {
-    width: 40%;
+    margin-top: .2rem;
+    width: 2.9rem;
   }
-  .img {
-    max-width: 100%;
+  .item__img {
+    display: block;
+    width: 100%;
+    height: 3rem;
+  }
+  .item__price {
+    margin-top: .1rem;
+    color: #f44;
+    font-family: arial;
   }
 </style>
