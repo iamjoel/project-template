2bc4ca2babad55941da85ef5f1fd08f71f1df7b1
joel <iamjoel007@gmail.com>
2018-05-16 18:24:53 +0800

    enchance

--- mobile/src/views/classify-list/Index.vue
+++ mobile/src/views/classify-list/Index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="main">
-    
+    <van-tabs v-model="active">
+      <van-tab  title="新品">
+        <div class="ly ly-multi ly-j item-wrap">
+          <div v-for="i in 9" :key="i" class="item">
+            <img src="http://via.placeholder.com/200x100" alt="" class="img" />
+            新品{{i}}
+          </div>
+          
+        </div>
+      </van-tab>
+      <van-tab  title="热销">
+        xx2
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
@@ -9,8 +22,8 @@
 export default {
   data() {
     return {
-      
-    };
+      active: 0
+    }
   },
   methods: {
     
@@ -18,4 +31,14 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+  .item-wrap {
+    padding: 0 3.3%;
+  }
+  .item {
+    width: 40%;
+  }
+  .img {
+    max-width: 100%;
+  }
+</style>
