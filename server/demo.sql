a355a31450f04304c63750f7a9b130ec5da4ee5d
zlwlogo <zlwlogo@hotmail.com>
2018-05-31 18:22:13 +0800

    edit file

---
+++ server/demo.sql
@@ -0,0 +1,46 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 阿里云-知行社
+Source Server Version : 50638
+Source Host           : 47.96.189.4:3306
+Source Database       : demo
+
+Target Server Type    : MYSQL
+Target Server Version : 50638
+File Encoding         : 65001
+
+Date: 2018-05-31 18:15:12
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for demo_category
+-- ----------------------------
+DROP TABLE IF EXISTS `demo_category`;
+CREATE TABLE `demo_category` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `detail` varchar(400) DEFAULT NULL,
+  `delFlg` int(1) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  `updateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for demo_item
+-- ----------------------------
+DROP TABLE IF EXISTS `demo_item`;
+CREATE TABLE `demo_item` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `detail` varchar(400) DEFAULT NULL,
+  `img` varchar(1000) DEFAULT NULL,
+  `categoryId` varchar(36) DEFAULT NULL,
+  `delFlg` int(1) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  `updateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
