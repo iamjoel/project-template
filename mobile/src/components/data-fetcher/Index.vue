0fadcd85b3c1df3ebb553c00213bbadec162dba2
joel <iamjoel007@gmail.com>
2018-06-12 17:12:15 +0800

    add data fetcher

---
+++ mobile/src/components/data-fetcher/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="data-fetcher">
+    <slot />
+  </div>
+</template>
+
+<script>
+import {fetchModel} from '@/service/api'
+export default {
+  props: {
+    config: Array
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    var len = this.config.length
+    var loaded = 0
+    var res = []
+
+    this.config.forEach((name, i) => {
+      fetchModel(c).then(({data})=> {
+        loaded++;
+        res[i] = data
+        if(loaded === len) {
+          this.$emit('loaded', res)
+        }
+      })
+    })
+  }
+}
+</script>
+
+<style scoped></style>
