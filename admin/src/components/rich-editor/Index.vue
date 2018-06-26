3a809306ff69719ae5cfa1ee5ce2db2566c16ea6
joel <iamjoel007@gmail.com>
2018-06-26 15:37:15 +0800

    add rich editor

---
+++ admin/src/components/rich-editor/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="editor">
+     <!-- 图片上传组件辅助-->
+    <el-upload class="image-uploader" name="file"
+             :action="addPicUrl" :show-file-list="false"
+             :on-success="imgLoaded">
+      <!-- <img v-if="model.img" :src="model.img | img" class="image-show">
+        <i v-else class="el-icon-plus image-uploader-icon"></i> -->
+    </el-upload>
+    <quill-editor v-model="content"
+        ref="myQuillEditor"
+        :options="editorOption"
+        @blur="onEditorBlur($event)"
+        @focus="onEditorFocus($event)"
+        @ready="onEditorReady($event)"
+        @change="onEditorChange">
+    </quill-editor>
+  </div>
+</template>
+
+<script src="./main.js">
+
+</script>
+
+<style scoped></style>
---
+++ vue-cli-admin/template/src/components/rich-editor/main.js
@@ -0,0 +1,89 @@
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
+
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{'header': 1}, {'header': 2}],               // custom button values
+  [{'list': 'ordered'}, {'list': 'bullet'}],
+  [{'script': 'sub'}, {'script': 'super'}],      // superscript/subscript
+  [{'indent': '-1'}, {'indent': '+1'}],          // outdent/indent
+  [{'direction': 'rtl'}],                         // text direction
+
+  [{'size': ['small', false, 'large', 'huge']}],  // custom dropdown
+  [{'header': [1, 2, 3, 4, 5, 6, false]}],
+
+  [{'color': []}, {'background': []}],          // dropdown with defaults from theme
+  [{'font': []}],
+  [{'align': []}],
+  ['link', 'image', 'video'],
+  ['clean']                                         // remove formatting button
+]
+
+import {IMGS_PREFIX} from '@/setting'
+
+export default {
+  props: {
+    value: null
+  },
+  components: {
+    quillEditor
+  },
+  data() {
+    return {
+      content: '',
+      editorOption: {
+          placeholder: '',
+          theme: 'snow',  // or 'bubble'
+          modules: {
+              toolbar: {
+                  container: toolbarOptions,  // 工具栏
+                  handlers: {
+                      'image': function (value) {
+                          if (value) {
+                            document.querySelector('.editor .image-uploader input').click()
+                          } else {
+                              this.quill.format('image', false);
+                          }
+                      }
+                  }
+              }
+          }
+      }
+    }  
+  },
+  watch: {
+  },
+  methods: {
+    onEditorBlur(quill) {
+      // console.log('editor blur!', quill)
+    },
+    onEditorFocus(quill) {
+      // console.log('editor focus!', quill)
+    },
+    onEditorReady(quill) {
+      // console.log('editor ready!', quill)
+    },
+    onEditorChange({ quill, html, text }) {
+      this.content = html
+      this.$emit('input', html)
+    },
+    imgLoaded(data) {
+      var img = `${IMGS_PREFIX}/${data.data}`
+      let quill = this.$refs.myQuillEditor.quill
+      // 获取光标所在位置
+      let length = quill.getSelection().index;
+      // 插入图片  res.info为服务器返回的图片地址
+      quill.insertEmbed(length, 'image', img)
+      // 调整光标到最后
+      quill.setSelection(length + 1)
+    },
+    setValue(value) {
+      this.content = value
+    }
+  }
+}
