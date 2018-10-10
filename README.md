2c6e60c0d042e5a558a0bd377ff218ac7153376f
joel <iamjoel007@gmail.com>
2018-10-10 11:11:45 +0800

    enchance struct

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 # 项目模板
 * 管理后台
   * Vue
-    * [源码](admin)。 [代码库](https://github.com/iamjoel/admin-codes-collection)。
-    * [脚手架](vue-cli-admin)。同步源码到脚手架: `gulp generator-admin`
+    * [源码](front-end/admin)。 [代码库](https://github.com/iamjoel/admin-codes-collection)。
+    * [脚手架](front-end/vue-cli-admin)。同步源码到脚手架: `gulp generator-admin`
     * [代码生成工具](https://iamjoel.github.io/admin-fe-generator/src/)
   * React
     * [react boilerplate](https://github.com/react-boilerplate/react-boilerplate)
-* [移动端](mobile)
+* [移动端](front-end/mobile)
   * Vue
-    * [源码](mobile)。 [代码库](https://github.com/iamjoel/mobile-codes-collection)。
-    * [脚手架](vue-cli-mobile)。同步源码到脚手架: `gulp generator-mobile`
+    * [源码](front-end/mobile)。 [代码库](https://github.com/iamjoel/mobile-codes-collection)。
+    * [脚手架](front-end/vue-cli-mobile)。同步源码到脚手架: `gulp generator-mobile`
   * React
     * [react boilerplate](https://github.com/react-boilerplate/react-boilerplate)
 * 小程序
-  * [WePY](wepy)
+  * [WePY](front-end/wepy)
 * 服务器端渲染(SSR)
-  * Nuxt.js [源码](nuxt)
+  * Nuxt.js [源码](front-end/nuxt)
 * PC 应用程序
   * [electron-vue](https://github.com/SimulatedGREG/electron-vue)
 * 服务器端
-  * [Node](server)
+  * [Node](server/node)
---
+++ server/node/config/config.default.js
@@ -0,0 +1,96 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_zhixingclub';
+
+  // 自定义中间件
+  //config.middleware = [ 'bodyparser', 'tokencheck', 'xmlbodytransfer'];
+  config.middleware = ['bodyparser', 'xmlbodytransfer'];
+
+  // log输出
+  config.logger = {
+    dir: './logs/server-template',
+    level: 'DEBUG',
+  };
+
+  // bodyParser设置
+  config.bodyParser = {
+    enable: false,
+  };
+
+  // 安全设置
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
+
+  // mysql设置
+  config.mysql = {
+    client: {
+      host: 'localhost',
+      port: '3306',
+      user: 'root',
+      password: '',
+      // 数据库名
+      database: 'admin-template',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
+
+  // cors设置
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+    credentials: true,
+  };
+
+  // 静态服务器设置
+  config.static = {
+    maxAge: 31536000,
+    dynamic: true,
+    preload: true,
+    buffer: true,
+    maxFiles: 10000,
+  };
+
+  //允许上传文件类型
+  config.multipart = {
+    fileSize:'10mb',
+    whitelist: [
+      // images
+      '.jpg',
+      '.jpeg', // image/jpeg
+      '.png', // image/png, image/x-png
+      '.gif', // image/gif
+      '.bmp', // image/bmp
+      // '.wbmp', // image/vnd.wap.wbmp
+      // '.webp',
+      // '.tif',
+      // '.psd',
+      // text
+      // '.svg',
+      // '.js', '.jsx',
+      // '.json',
+      // '.css', '.less',
+      // '.html', '.htm',
+      // '.xml',
+      // tar
+      '.zip',
+      // '.gz', '.tgz', '.gzip',
+      // video
+      '.mp3',
+      '.wma',
+      '.mp4',
+      '.avi',
+    ]
+  };
+
+  return config;
+};
