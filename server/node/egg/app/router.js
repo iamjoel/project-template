e40a264f3d330cc0721bd97d4274858718e93163
joel <iamjoel007@gmail.com>
2018-10-22 16:43:22 +0800

    enchance router

--- server/node/egg/app/router.js
+++ server/node/egg/app/router.js
@@ -1,17 +1,23 @@
-var resources = ['category']
+var singleTable = ['category']
 module.exports = app => {
   const { router, controller } = app
-
+  
+  // 单表的CRUD
+  singleTable.forEach(tableName => {
+    addCRUD(tableName, router, controller.common.index)
+  })
+  
+  // 多表的CRUD
   router.get(`/api/item/list`, controller.item.item.list)
   router.get(`/api/item/detail/:id`, controller.item.item.detail)
 
-  // 通用的CRUD
-  const commonController = controller.common.index
-  resources.forEach(resourceName => {
-    router.get(`/api/${resourceName}/detail/:id`, commonController.detail)
-    router.get(`/api/${resourceName}/list`, commonController.list)
-    router.post(`/api/${resourceName}/add`, commonController.add)
-    router.post(`/api/${resourceName}/edit`, commonController.edit)
-    router.post(`/api/${resourceName}/del/:id`, commonController.del)
-  })
+  
+}
+
+function addCRUD(tableName, router, commonController) {
+  router.get(`/api/${tableName}/detail/:id`, commonController.detail)
+  router.get(`/api/${tableName}/list`, commonController.list)
+  router.post(`/api/${tableName}/add`, commonController.add)
+  router.post(`/api/${tableName}/edit`, commonController.edit)
+  router.post(`/api/${tableName}/del/:id`, commonController.del)
 }
