3a53fb586d7ea2c222f1a32eefbb208d04a43ded
joel <iamjoel007@gmail.com>
2018-11-01 18:24:56 +0800

    enchance

--- server/node/gulpfile.js
+++ server/node/gulpfile.js
@@ -33,7 +33,7 @@ function mv(type) {
     `!${srcRootPath}/.idea`,
     `!${srcRootPath}/logs`,
     `!${srcRootPath}/logs/**/*`,
-    `!${srcRootPath}/db.sql`,
+    `!${srcRootPath}/TODO.md`,
     `!${srcRootPath}/yarn.lock`,
     // ...hasVueFolderPath,  // 会在 replace 中做替换
     ])
