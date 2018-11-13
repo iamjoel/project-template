d8abcdd9efe41434c9afcc02282a76c1ecc6a464
joel <iamjoel007@gmail.com>
2018-11-13 19:49:05 +0800

    add upload file

--- server/node/egg/app/controller/common/upload.js
+++ server/node/egg/app/controller/common/upload.js
@@ -10,6 +10,17 @@ class UploadController extends Controller {
       ctx.body = ctx.fail(-1, e);
     }
   }
+
+  async uploadFile() {
+    const { ctx, service, config } = this;
+    try {
+      const stream = await ctx.getFileStream();
+      const res = await service.common.upload.uploadFile(stream);
+      ctx.body = ctx.success(res);
+    } catch (e) {
+      ctx.body = ctx.fail(-1, e);
+    }
+  }
 }
 
 module.exports = UploadController;
