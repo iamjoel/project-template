f3e987d5dbf94ae0aa95ff91fe59ca969a811c4d
joel <iamjoel007@gmail.com>
2019-05-06 17:18:44 +0800

    记录每次的 api 的简化写法

--- server/node/egg-boilerplate/boilerplate/app/middleware/log.js
+++ server/node/egg-boilerplate/boilerplate/app/middleware/log.js
@@ -3,56 +3,62 @@ const config = require('../../config/config.default.js')({});
 
 module.exports = app => {
   return async function(ctx, next) {
-    var requestTime = new Date();
-    await next();
-    var responseTime = new Date();
+    // 记录每次 api
+    await next()
+
+    ctx.logger.info(`
+params: ${decodeURIComponent((ctx.captures && ctx.captures.length > 0) ? ctx.captures[0] : ctx.request.querystring)}
+body: ${decodeURIComponent(JSON.stringify(ctx.request.body))}
+    `)
+    // var requestTime = new Date();
+    // await next();
+    // var responseTime = new Date();
 
 
-    // api log
-    if(!ctx.request.ctx.URL.pathname.includes('log') && !ctx.request.ctx.URL.pathname.includes('export') ) {
-        var equip = ctx.query.isAdminEnd == 1 ? 'admin' : 'mobile' // 设备，admin 或者 mobile。
-        var errMsg = (ctx.response.body && ctx.response.body.errorCode != 0 && ctx.response.body.errorCode !=999 && ctx.response.body.errorCode!=undefined)
-                ? ctx.response.body.errorMessage.replace('Error:', '').trim() : null
-        var isError = !!errMsg
-        const insertData = {
-            ip: ctx.request.ip,
-            userAgent: ctx.request.header["user-agent"],
-            equip,
-            url: ctx.request.ctx.URL.pathname.replace(/detail\/[0-9|a-z|A-Z|-]+/g, 'detail/'),
-            params: (ctx.captures && ctx.captures.length > 0) ? ctx.captures[0] : ctx.request.querystring,
-            method: ctx.request.method,
-            body: JSON.stringify(ctx.request.body),
-            response: JSON.stringify(ctx.response.body),
-            isError,
-            errMsg,
-            requestTime: requestTime,
-            responseTime: responseTime
-        };
+    // // api log
+    // if(!ctx.request.ctx.URL.pathname.includes('log') && !ctx.request.ctx.URL.pathname.includes('export') ) {
+    //     var equip = ctx.query.isAdminEnd == 1 ? 'admin' : 'mobile' // 设备，admin 或者 mobile。
+    //     var errMsg = (ctx.response.body && ctx.response.body.errorCode != 0 && ctx.response.body.errorCode !=999 && ctx.response.body.errorCode!=undefined)
+    //             ? ctx.response.body.errorMessage.replace('Error:', '').trim() : null
+    //     var isError = !!errMsg
+    //     const insertData = {
+    //         ip: ctx.request.ip,
+    //         userAgent: ctx.request.header["user-agent"],
+    //         equip,
+    //         url: ctx.request.ctx.URL.pathname.replace(/detail\/[0-9|a-z|A-Z|-]+/g, 'detail/'),
+    //         params: (ctx.captures && ctx.captures.length > 0) ? ctx.captures[0] : ctx.request.querystring,
+    //         method: ctx.request.method,
+    //         body: JSON.stringify(ctx.request.body),
+    //         response: JSON.stringify(ctx.response.body),
+    //         isError,
+    //         errMsg,
+    //         requestTime: requestTime,
+    //         responseTime: responseTime
+    //     };
 
-        await ctx.service.log.apiLog.add('api_log', insertData)
-    }
+    //     await ctx.service.log.apiLog.add('api_log', insertData)
+    // }
 
-    if(
-        ctx.request.ctx.URL.pathname.includes('add')
-        || ctx.request.ctx.URL.pathname.includes('edit')
-        || ctx.request.ctx.URL.pathname.includes('audit')
-        || ctx.request.ctx.URL.pathname.includes('del')
-        // || true
-    ) {
-      const userInfo = tokenTools.getUserInfoByToken(ctx.req.headers.token, config.jwtTokenSecret);
-      var pathArr = ctx.request.path.split('/')
-      var dataId = getDataId(pathArr[3], pathArr, ctx.request.body)
-      console.log(dataId)
-      var res = await ctx.service.log.opLog.add('op_log', {
-        accountId: userInfo.id,
-        tableName: pathArr[2],
-        dataId,
-        action: toAction(pathArr[3]),
-        params: ctx.request.body ? JSON.stringify(ctx.request.body) : '-',
-      })
+    // if(
+    //     ctx.request.ctx.URL.pathname.includes('add')
+    //     || ctx.request.ctx.URL.pathname.includes('edit')
+    //     || ctx.request.ctx.URL.pathname.includes('audit')
+    //     || ctx.request.ctx.URL.pathname.includes('del')
+    //     // || true
+    // ) {
+    //   const userInfo = tokenTools.getUserInfoByToken(ctx.req.headers.token, config.jwtTokenSecret);
+    //   var pathArr = ctx.request.path.split('/')
+    //   var dataId = getDataId(pathArr[3], pathArr, ctx.request.body)
+    //   console.log(dataId)
+    //   var res = await ctx.service.log.opLog.add('op_log', {
+    //     accountId: userInfo.id,
+    //     tableName: pathArr[2],
+    //     dataId,
+    //     action: toAction(pathArr[3]),
+    //     params: ctx.request.body ? JSON.stringify(ctx.request.body) : '-',
+    //   })
 
-      // console.log(res)
-    }
+    // }
 
   };
 };
