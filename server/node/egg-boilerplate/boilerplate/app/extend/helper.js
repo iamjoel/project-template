d0fa984a0d5fd004cd30976964d87ffe3754459e
joel <iamjoel007@gmail.com>
2019-06-19 10:38:27 +0800

    样式优化和优化bug

--- server/node/egg-boilerplate/boilerplate/app/extend/helper.js
+++ server/node/egg-boilerplate/boilerplate/app/extend/helper.js
@@ -1,5 +1,5 @@
 module.exports = {
-  getFields: require('@/util/fields/get-fields'),
-  checkFields: require('@/util/fields/check-fields'),
+  getFields: require('../util/fields/get-fields'),
+  checkFields: require('../util/fields/check-fields'),
   uuid: require('uuid'),
 };
